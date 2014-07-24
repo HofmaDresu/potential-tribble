544fcb1dc56824d410c3bbcaba15dfd50bc3d0c0
Matt Hoffman <hofmadresu@gmail.com>
2014-07-23 22:45:34 -0400

    Add http dependency to project

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
 (defproject potential-tribble "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Small desktop application to convrt Wide World's website's team schedule to an importable calendar format"
+  :url ""
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [clj-http "0.9.2"]]
   :main ^:skip-aot potential-tribble.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
