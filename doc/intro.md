3f89dfbdd33d17ad7a00f99afe0e4295bcbced54
Matt Hoffman <hofmadresu@gmail.com>
2014-07-23 22:24:38 -0400

    Create initial clojure project from leiningen

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to potential-tribble
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/potential_tribble/core_test.clj
@@ -0,0 +1,7 @@
+(ns potential-tribble.core-test
+  (:require [clojure.test :refer :all]
+            [potential-tribble.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
