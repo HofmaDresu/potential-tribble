f1ab2844200c8ddcba01da18eb49377f69d9814b
Matt Hoffman <hofmadresu@gmail.com>
2014-08-04 19:29:43 -0400

    Add get for wideworld schedule home and store body

--- src/potential_tribble/core.clj
+++ src/potential_tribble/core.clj
@@ -1,7 +1,18 @@
 (ns potential-tribble.core
+  (:require [clj-http.client :as http])
   (:gen-class))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(def wideworld-schedule-home "http://secure.hammerweb.net/wws_membership/SchedulesScoresDisplay.asp")
+
+(defn get-page-body [url]
+  (:body (http/get url)))
+
+(defn get-wideworld-schedule-home []
+  (get-page-body wideworld-schedule-home))
+
+(def wideworld-schedule-home-body (get-wideworld-schedule-home))
