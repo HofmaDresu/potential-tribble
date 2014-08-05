0002e632b75379db6e228d4fcd1e6fffc81f5c62
Matt Hoffman <hofmadresu@gmail.com>
2014-08-04 20:36:18 -0400

    switch to enlive. pull seasons from page

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [clj-http "0.9.2"]]
+                 [enlive "1.1.5"]]
   :main ^:skip-aot potential-tribble.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/potential_tribble/core.clj
+++ src/potential_tribble/core.clj
@@ -1,5 +1,6 @@
 (ns potential-tribble.core
-  (:require [clj-http.client :as http])
+  (:require [net.cgrand.enlive-html :as html])
+  (:require [clojure.string :as cstr])
   (:gen-class))
 
 (defn -main
@@ -7,12 +8,24 @@
   [& args]
   (println "Hello, World!"))
 
-(def wideworld-schedule-home "http://secure.hammerweb.net/wws_membership/SchedulesScoresDisplay.asp")
+(def wideworld-schedule-home-url "http://secure.hammerweb.net/wws_membership/SchedulesScoresDisplay.asp")
 
-(defn get-page-body [url]
-  (:body (http/get url)))
+(defn get-page [url]
+  (html/html-resource (java.net.URL. url)))
 
 (defn get-wideworld-schedule-home []
-  (get-page-body wideworld-schedule-home))
+  (get-page wideworld-schedule-home-url))
 
-(def wideworld-schedule-home-body (get-wideworld-schedule-home))
+(def wideworld-schedule-home (get-wideworld-schedule-home))
+
+(def seasons
+  (let [potential-seasons (html/select wideworld-schedule-home [:p.style81])]
+    ; Each potential season has a content mapping. Unfortunatly, there isn't a good way to differentiate between seasons
+    ; and the Archived Standings links. Hence, the rest:
+    (let [potential-season-content (map :content potential-seasons)]
+      ; The only way that I've found to find the difference between seasons and the Archive standings is that 
+      ; seasons have more first-level sub items than the link
+      (let [season-strings (map last (filter #(>= (count %) 2) potential-season-content))]
+        ; Seasons have a line break in them. I don't really want that hanging around, so split it out
+        (map #(first (cstr/split % #"\n")) season-strings)))))
+                                        
